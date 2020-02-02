afac7b3096b29c4c7d0cc17d951076d340474e0a
Eddcakes <eddadamson@gmail.com>
2020-02-02 22:12:58 +0000

    got popup working with sync storage

--- popup.js
+++ popup.js
@@ -1,25 +1,31 @@
+/* 
+if in popup.html script is added before dom elements we are trying to find
+we would need to rap our code inside DOMContentLoaded
+document.addEventListener('DOMContentLoaded', ()=>{ content here });
+*/
+
+//sample data incase have to initialise chrome storage
+// ['FRENCH LIGUE 1', 'ITALIAN COPPA ITALIA', 'CHAMPIONSHIP']
+
 let textArea = document.querySelector('#compList');
+let saveBtn = document.querySelector('#saveComp');
+saveBtn.addEventListener('click', evt => saveList());
 
-/*  chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-}); */
-
-function saveComps() {
-  let userItems = document.querySelector('#compList');
-  console.log(userItems.value);
-  chrome.storage.sync.set({ competitions: userItems.value }, () => {
-    console.log('set value to: ' + competitions.value);
-  });
-  //chrome.storage.sync.set()
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  chrome.storage.sync.get('userItems', result => {
-    console.log(result);
-    textArea.value = result.competitions;
-  });
-  let saveBtn = document.getElementById('saveComp');
-  console.log(saveBtn);
-  saveBtn.addEventListener('click', () => saveComps());
+chrome.storage.sync.get('compList', data => {
+  //data is stored as an array
+  if (data !== undefined) {
+    // console.log(data.compList);
+    textArea.value = data.compList.join('\n');
+  } else {
+    textArea.value = '';
+  }
 });
+
+function saveList() {
+  let textArea = document.querySelector('#compList');
+  let compArray = textArea.value.split('\n');
+  console.log(compArray);
+  chrome.storage.sync.set({ compList: compArray }, () => {
+    console.log('added values');
+  });
+}
