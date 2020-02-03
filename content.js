30bdd1f25b413db0ccc2943db3c41662a1373f96
Eddcakes <eddadamson@gmail.com>
2020-02-03 01:11:00 +0000

    reordering seems to be working

--- content.js
+++ content.js
@@ -1,39 +1,49 @@
-document.addEventListener('readystatechange', evt => {
-  console.log(document.readyState);
-  console.log('state changed');
-  // not All as we want to get parent node anyway
-
-  // observer seems to work when i enter the code into the console
-  // why is it not working from the extension :(
-  const list = document.querySelector('.qa-match-block').parentNode;
-
-  const observer = new MutationObserver(mutations => {
-    mutations.forEach(record => {
-      console.log(record);
-    });
-  });
-  console.log(list);
-  observer.observe(list, {
-    attributes: true,
-    childList: true,
-    subtree: true
-  });
-});
-
+// wait for msg from background
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log(request, sender, sendResponse);
   if (request.args === 'pageUpdate') {
     console.log('page has been updated');
+    sortList();
   }
   sendResponse({ response: 'pageUpdate received' });
 });
 
-chrome.storage.sync.get('compList', data => {
-  //data is stored as an array
-  console.log(data.compList);
-  const favLeagues = data.compList;
-  // now we can search the node list to try and reorder
-});
-
-// .qa-match-block is the competition dom
-//mutation observers
+function sortList() {
+  chrome.storage.sync.get('compList', data => {
+    //data is stored as an array
+    const favLeagues = data.compList;
+    console.log(favLeagues);
+    // now we can search the node list to try and reorder
+    let list = document.querySelector('.qa-match-block').parentNode;
+    const todaysComps = document.querySelectorAll('.qa-match-block');
+    let compArray = [...todaysComps];
+    //console.log(favLeagues, list);
+    let todayFav = [];
+    let remainder = [];
+    for (let compIndex = 0; compIndex < todaysComps.length; compIndex++) {
+      let added = false;
+      for (let favIndex = 0; favIndex < favLeagues.length; favIndex++) {
+        if (
+          compArray[compIndex].children[0].innerText.trim() ===
+          favLeagues[favIndex]
+        ) {
+          todayFav.push(compArray[compIndex]);
+          added = true;
+          break;
+        }
+      }
+      if (!added) {
+        remainder.push(compArray[compIndex]);
+      }
+    }
+    const newOrder = [...todayFav, ...remainder];
+    // console.log(todayFav, remainder);
+    // console.log(newOrder);
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
+    }
+    newOrder.forEach(element => {
+      list.appendChild(element);
+    });
+  });
+}
